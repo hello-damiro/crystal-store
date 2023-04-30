3b4dd4ff60644f708a399e164b9f6c93ff8defaf
damiro <hello.damiro@gmail.com>
2023-04-30 23:38:21 +0800

    crystal component done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import Crystal from './components/shared/Crystal';
 function App() {
     return (
         <div className="App">
-            <Crystal color="red" />
-            <Crystal color="magenta" />
-            <Crystal color="green" />
+            <Crystal color="#C42D2D" width="120" meaning="fertility" />
+            <Crystal color="#EEC900" width="150" meaning="breakthrough" />
+            <Crystal color="#6A0DAD" meaning="flow" shadowed="true" shadowGap="32" />
         </div>
     );
 }
--- src/components/shared/Crystal.jsx
+++ src/components/shared/Crystal.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
-function Crystal({ color }) {
-    return <div className="crystal" style={{ backgroundColor: color }}></div>;
+function Crystal({ color, width, meaning, shadowed, shadowGap }) {
+    return (
+        <div className="crystal-piece" style={{ width: width + 'px', gap: shadowGap + 'px' }}>
+            <div
+                className={`crystal ${meaning}`}
+                style={{ backgroundColor: color, width: width + 'px' }}
+            />
+            <div className={`${shadowed ? 'shadow' : ''}`}></div>
+        </div>
+    );
 }
+
+Crystal.defaultProps = {
+    width: 100,
+    meaning: '',
+    shadowed: false,
+    shadowGap: 0,
+};
 
 export default Crystal;
