cadf52967e354925d8a16e83ed01fdd18f31f858
damiro <hello.damiro@gmail.com>
2023-04-30 22:36:47 +0800

    prepared files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Crystal from './components/shared/Crystal';
 function App() {
-    return <div className="App">App</div>;
+    return (
+        <div className="App">
+            <Crystal color="red" />
+            <Crystal color="magenta" />
+            <Crystal color="green" />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/shared/Crystal.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Crystal({ color }) {
+    return <div className="crystal" style={{ backgroundColor: color }}></div>;
+}
+
+export default Crystal;
