fb461b1c0e4b0f49ea841bea55397c45627ef2bf
damiro <hello.damiro@gmail.com>
2023-05-05 15:26:40 +0800

    bug with naming an object

--- src/components/hooks/CartReducer.js
+++ src/components/hooks/CartReducer.js
@@ -14,10 +14,10 @@ export function cartReducer(cartItems, action) {
             return cartItems.filter((item) => item.crystal.id !== action.payload);
         }
         case ACTIONS.ADD: {
-            const foundItem = collection.find((crystal) => crystal.id === action.payload);
+            const crystal = collection.find((crystal) => crystal.id === action.payload);
             const item = cartItems.filter((item) => item.crystal.id === action.payload);
             if (item.length === 0) {
-                cartItems = [...cartItems, { foundItem, count: 1 }];
+                cartItems = [...cartItems, { crystal, count: 1 }];
             } else {
                 cartItems = cartItems.map((item) =>
                     item.crystal.id === action.payload ? { ...item, count: item.count + 1 } : item
