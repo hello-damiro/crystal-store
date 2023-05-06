fdcd9f8fa575d03d7c68708dc7aa4183959aa490
damiro <hello.damiro@gmail.com>
2023-05-06 16:53:45 +0800

    styled for mobile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Ribbon from './components/Ribbon';
 import Header from './components/Header';
 import Main from './components/Main';
